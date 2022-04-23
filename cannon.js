501c81b487b1e85c2486e9456cd56c81e119ecb6
mamtawardhani <53176352+mamtawardhani@users.noreply.github.com>
2022-04-23 17:22:16 +0530

    Add files via upload

---
+++ cannon.js
@@ -0,0 +1,19 @@
+class Cannon{
+    constructor(x, y, width,height, angle){
+        this.x = x
+        this.y = y
+        this.w = width
+        this.h = height
+        this.a = angle
+    }
+
+    display(){
+        push()
+        translate(this.x, this.y)
+        rotate(this.a)
+        rect(-10,-20,this.w, this.h)
+        pop()
+        arc(this.x-30, this.y+90, 140, 160, PI, TWO_PI)
+    }
+
+}
